7b4a33ab68fc5ab9d44fa9e03f5e1cb4c62741d8
raodeming <raodeming@anji-plus.com>
2020-04-14 15:56:47 +0800

    add company info

--- core/captcha/src/main/java/com/anji/captcha/service/impl/CaptchaRedisServiceImpl.java
+++ core/captcha/src/main/java/com/anji/captcha/service/impl/CaptchaRedisServiceImpl.java
@@ -1,3 +1,9 @@
+/*
+ *Copyright © 2018 anji-plus
+ *安吉加加信息技术有限公司
+ *http://www.anji-plus.com
+ *All rights reserved.
+ */
 package com.anji.captcha.service.impl;
 
 import com.anji.captcha.service.CaptchaRedisService;
