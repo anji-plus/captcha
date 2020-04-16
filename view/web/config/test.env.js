2810eb9f3b59e79d36f5d068c1ba9df8bfad7581
wemooly <525507670@qq.com>
2020-04-16 19:34:26 +0800

    分环境打包配置

--- view/web/config/test.env.js
+++ view/web/config/test.env.js
@@ -1,6 +1,7 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+
 module.exports = merge(prodEnv, {
   NODE_ENV: '"testing"',
   BASE_API: '"http://10.108.6.131"'
