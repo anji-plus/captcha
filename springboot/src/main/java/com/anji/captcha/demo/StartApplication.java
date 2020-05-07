47f4ded53d1e25a62435d7b3a3016e89a041834b
lide1202 <lide1202@hotmail.com>
2020-05-07 17:58:28 +0800

    add spring boot demo

---
+++ springboot/src/main/java/com/anji/captcha/demo/StartApplication.java
@@ -0,0 +1,20 @@
+/*
+ *Copyright © 2018 anji-plus
+ *安吉加加信息技术有限公司
+ *http://www.anji-plus.com
+ *All rights reserved.
+ */
+package com.anji.captcha.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.anji.captcha"})
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
