3ea29327abdaf38a8e725ca60e59cec70bc24992
lide1202 <lide1202@hotmail.com>
2020-05-07 17:57:55 +0800

    delete redis dependency

---
+++ core/captcha/src/main/java/com/anji/captcha/util/CacheUtil.java
@@ -0,0 +1,43 @@
+package com.anji.captcha.util;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public final class CacheUtil {
+
+    private static final Map<String, Object> CACHE_MAP = new ConcurrentHashMap<String, Object>();
+
+    public static void set(String key, String value){
+
+    }
+
+    public static void set(String key, String value, long expiresInSeconds){
+        CACHE_MAP.put(key, value);
+        CACHE_MAP.put(key + "_HoldTime", System.currentTimeMillis() + expiresInSeconds*1000);//缓存失效时间
+    }
+
+    public static void delete(String key){
+        CACHE_MAP.remove(key);
+        CACHE_MAP.remove(key + "_HoldTime");
+    }
+
+    public static boolean exists(String key){
+        Long cacheHoldTime = (Long) CACHE_MAP.get(key + "_HoldTime");
+        if (cacheHoldTime == null || cacheHoldTime == 0L) {
+            return false;
+        }
+        if (cacheHoldTime < System.currentTimeMillis()) {
+            delete(key);
+            return false;
+        }
+        return true;
+    }
+
+
+    public static String get(String key){
+        if (exists(key)) {
+            return (String)CACHE_MAP.get(key);
+        }
+        return null;
+    }
+}
