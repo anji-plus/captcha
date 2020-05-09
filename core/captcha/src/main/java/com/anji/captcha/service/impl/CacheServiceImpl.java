b041d98b13e2810b77a24db56bfe6f462eed420d
lide1202 <lide1202@hotmail.com>
2020-05-09 14:21:31 +0800

    Update CacheServiceImpl.java

--- core/captcha/src/main/java/com/anji/captcha/service/impl/CacheServiceImpl.java
+++ core/captcha/src/main/java/com/anji/captcha/service/impl/CacheServiceImpl.java
@@ -8,38 +8,53 @@ package com.anji.captcha.service.impl;
 
 import com.anji.captcha.service.CacheService;
 import com.anji.captcha.util.CacheUtil;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import java.util.concurrent.TimeUnit;
+/**
+ *
+ */
 @Service
 public class CacheServiceImpl implements CacheService {
 
-	//@Autowired
-	//private StringRedisTemplate stringRedisTemplate;
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
 
 	@Override
 	public void set(String key, String value, long expiresInSeconds) {
-		CacheUtil.set(key, value, expiresInSeconds);
-		//stringRedisTemplate.opsForValue().set(key, value, expiresInSeconds, TimeUnit.SECONDS);
+		if(stringRedisTemplate != null){
+			stringRedisTemplate.opsForValue().set(key, value, expiresInSeconds, TimeUnit.SECONDS);
+		}else{
+			CacheUtil.set(key, value, expiresInSeconds);
+		}
 	}
 
 	@Override
 	public boolean exists(String key) {
-		return CacheUtil.exists(key);
-		//return stringRedisTemplate.hasKey(key);
+		if(stringRedisTemplate != null){
+			return stringRedisTemplate.hasKey(key);
+		}else{
+			return CacheUtil.exists(key);
+		}
 	}
 
 	@Override
 	public void delete(String key) {
-		CacheUtil.delete(key);
-		//stringRedisTemplate.delete(key);
+		if(stringRedisTemplate != null){
+			stringRedisTemplate.delete(key);
+		}else{
+			CacheUtil.delete(key);
+		}
 	}
 
 	@Override
 	public String get(String key) {
-		return CacheUtil.get(key);
-		// return stringRedisTemplate.opsForValue().get(key);
+		if(stringRedisTemplate != null){
+			return stringRedisTemplate.opsForValue().get(key);
+		}else{
+			return CacheUtil.get(key);
+		}
 	}
 
 }
