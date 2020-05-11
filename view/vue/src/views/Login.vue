2cbae0ec49e72561b1a9159ad656262309e40c08
lixiaoyan <lixiaoyan@anji-plus.com>
2020-05-11 14:59:54 +0800

    链接修改

--- view/vue/src/views/Login.vue
+++ view/vue/src/views/Login.vue
@@ -50,6 +50,16 @@
                             <!-- <el-button class="forget fr" @click="goForgetPassword">忘记密码?</el-button> -->
 
                           </li>
+                           <li class="keep-password">
+                            <label>
+                              <a href='https://gitee.com/anji-plus/captcha/invite_link?invite=7df099e79d3373008f25296f59e36cfb033e7bd786211623b7993663152babc589bab2c87e156b5ae766b0d826817bc9' target="_blank" style="color:#222"><img src="https://gitee.com/static/images/logo-en.svg" style="vertical-align:middle;margin-right:5px" width="24px"/> gitee</a>
+                            </label>
+
+                            <label class="ml20">
+                             <a href='https://gitee.com/anji-plus/captcha/stargazers' target="_blank"><img src='https://gitee.com/anji-plus/captcha/badge/star.svg?theme=gvp' style="vertical-align:middle;" alt='star' /></a>
+                            </label>
+                        
+                          </li>
                           <li class="mt50">
                             <!-- @click="login" -->
                             <el-button class="buttonSize button-solid goHome" type="primary" @click="checkPrama">
