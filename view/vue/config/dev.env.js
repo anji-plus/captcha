90e9d657196d50935be014ec3af559198b2f79b4
lide1202 <lide1202@hotmail.com>
2020-05-12 12:22:17 +0800

    update for view vue

--- view/vue/config/dev.env.js
+++ view/vue/config/dev.env.js
@@ -4,6 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://10.108.11.46/api"'
-  // BASE_API: '"http://127.0.0.1:8086/"'
+  BASE_API: '"http://localhost:8080"'
 })
--- view/vue/config/test.env.js
+++ view/vue/config/test.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"testing"',
-  BASE_API: '"http://10.108.6.131/api"'
+  BASE_API: '"http://localhost:8080"'
 })
