8e003e30f35d0465797f871a8793f3abfe2ac1cf
raodeming <1130305001@qq.com>
2020-05-13 13:25:43 +0800

    springboot login示例

--- service/springboot/src/main/java/com/anji/captcha/demo/controller/DemoController.java
+++ service/springboot/src/main/java/com/anji/captcha/demo/controller/DemoController.java
@@ -11,20 +11,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by raodeming on 2020/5/13.
+ * 后端二次校验接口示例
  */
 @RestController
 @RequestMapping("/test")
 public class DemoController {
 
     @Autowired
-    @Lazy  //懒加载
+    @Lazy
     private CaptchaService captchaService;
 
-    @PostMapping("/get")
+    @PostMapping("/login")
     public ResponseModel get(@RequestBody CaptchaVO captchaVO) {
-        ResponseModel responseModel = captchaService.get(captchaVO);
-        return responseModel;
+        ResponseModel response = captchaService.verification(captchaVO);
+        if(response.isSuccess() == false){
+            //验证码校验失败，返回信息告诉前端
+            //repCode  0000  无异常，代表成功
+            //repCode  9999  服务器内部异常
+            //repCode  0011  参数不能为空
+            //repCode  6110  验证码已失效，请重新获取
+            //repCode  6111  验证失败
+            //repCode  6112  获取验证码失败,请联系管理员
+        }
+        return response;
     }
 
 }
