38372188d4a8d0b23649ac3d0fdf6e42bffb9539
wuyan <wuyan@anji_allways.com>
2020-05-13 17:14:55 +0800

    opt：请求地址和请求参数修改

--- view/android/VerificationCodeDemo/app/src/main/java/com/example/verificationcodedemo/network/CommonInterceptor.kt
+++ view/android/VerificationCodeDemo/app/src/main/java/com/example/verificationcodedemo/network/CommonInterceptor.kt
@@ -24,21 +24,22 @@ class CommonInterceptor(private val cx: Context) : Interceptor {
 
         val originalJsonElement = JsonParser().parse(getParamContent(request?.body()!!))
 
-        val time = System.currentTimeMillis().toString()
-        val baseSignMsg = ("reqData" + originalJsonElement + "time" + time
-                + "token" + Configuration.token)
-
-        val newJsonObject = JsonObject()
-
-        newJsonObject.addProperty("time", time)
-        newJsonObject.addProperty("token", Configuration.token)
-        newJsonObject.add("reqData", originalJsonElement)
-        newJsonObject.addProperty("sign", MD5Util.encode(baseSignMsg))
-
-        Log.e("请求参数", newJsonObject.toString())
+//        val time = System.currentTimeMillis().toString()
+//        val baseSignMsg = ("reqData" + originalJsonElement + "time" + time
+//                + "token" + Configuration.token)
+//
+//        val newJsonObject = JsonObject()
+//
+//        newJsonObject.addProperty("time", time)
+//        newJsonObject.addProperty("token", Configuration.token)
+//        newJsonObject.add("reqData", originalJsonElement)
+//        newJsonObject.addProperty("sign", MD5Util.encode(baseSignMsg))
+//
+//        Log.e("请求参数", newJsonObject.toString())
+        Log.e("请求参数", originalJsonElement.toString())
 
         val newRequestBody =
-            RequestBody.create(request.body()!!.contentType(), newJsonObject.toString())
+            RequestBody.create(request.body()!!.contentType(), originalJsonElement.toString())
 
         val newRequest = request.newBuilder()
             .header("Accept-Language", "zh-cn,zh")
