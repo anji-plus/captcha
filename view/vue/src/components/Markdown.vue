0c3a509d97fccc9fef6313a7d4703f00215a9e49
wemoo <525507670@qq.com>
2020-05-14 16:18:00 +0800

    在线文档平台 markdowm编辑统一化  uni-app 提交disk文件夹

---
+++ view/vue/src/components/Markdown.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="markdown">
+        <mavon-editor v-model="value"></mavon-editor>
+    </div>
+</template>
+
+<script>
+
+import {readFile} from './../utils/readFile'
+
+export default {
+    props:{
+        filePath:{
+            type:String,
+            require:true
+        }
+    },
+    data() {
+        return {
+            value:'',
+        }
+    },
+    watch: {
+        filePath:'loadFile',
+        immediate:true
+    },
+    methods: {
+        loadFile(){
+            readFile(this.filePath).then(res=>{
+                this.value = res.data ? res.data: res
+            })
+        }
+    },
+    created() {
+        this.loadFile()
+    },
+}
+</script>
+
+<style lang="less" scoped>
+/deep/.v-note-wrapper {
+    height:calc(100vh - 186px)!important;
+    .v-note-op{
+        display: none!important;
+    }
+    .v-note-edit{
+        display: none!important;
+    }
+    .v-note-show{
+        width: 100%!important;
+        flex: 0 0 100%!important;
+    }
+} 
+</style>
