0963d49133c1a581617126c4bfca9ec7dd2caff6
raodeming <1130305001@qq.com>
2020-05-31 09:59:45 +0800

    spring-boot-captcha-starter版本，待更新

---
+++ core/captcha-spring-boot-starter/src/main/java/com/anji/captcha/demo/controller/CaptchaController.java
@@ -0,0 +1,44 @@
+/*
+ *Copyright © 2018 anji-plus
+ *安吉加加信息技术有限公司
+ *http://www.anji-plus.com
+ *All rights reserved.
+ */
+package com.anji.captcha.demo.controller;
+
+import com.anji.captcha.model.common.ResponseModel;
+import com.anji.captcha.model.vo.CaptchaVO;
+import com.anji.captcha.service.CaptchaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by raodeming on 2019/12/25.
+ */
+@RestController
+@RequestMapping("/captcha")
+public class CaptchaController {
+
+    @Autowired
+    private CaptchaService captchaService;
+
+    @PostMapping("/get")
+    public ResponseModel get(@RequestBody CaptchaVO captchaVO) {
+        return captchaService.get(captchaVO);
+    }
+
+    @PostMapping("/check")
+    public ResponseModel check(@RequestBody CaptchaVO captchaVO) {
+        return captchaService.check(captchaVO);
+    }
+
+    @PostMapping("/verify")
+    public ResponseModel verify(@RequestBody CaptchaVO captchaVO) {
+        return captchaService.verification(captchaVO);
+    }
+
+
+}
