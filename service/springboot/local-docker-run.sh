73c857a01f882be6383ccb525b79a16180f5eae8
wangbin <wangbin@anji-plus.com>
2024-12-26 13:09:05 +0800

    response支持国际化test

--- service/springboot/local-docker-run.sh
+++ service/springboot/local-docker-run.sh
@@ -3,6 +3,9 @@
 docker stop captcha-v2
 docker rm captcha-v2
 docker rmi captcha-service:v2
+ENV LANG=zh_CN.UTF-8 \
+    LANGUAGE=zh_CN:zh \
+    LC_ALL=zh_CN.UTF-8
 
 docker build -t captcha-service:v2 .
 
@@ -11,17 +14,19 @@ docker run -d -p 8082:8080 --name captcha-v2 captcha-service:v2
 sleep 5
 echo "http://localhost:8082/captcha/get"
 
-curl -X POST http://localhost:8082/captcha-api/captcha/get \
+curl -X POST http://localhost:8082/captcha/get \
    -d '{"captchaType":"clickWord"}' \
    -H "Content-Type: application/json" \
    -H "accept-language: en-US"
 
+echo '\r\ntest-check-1,en_US'
 curl -X POST http://localhost:8082/captcha/check    \
-   -d '{"captchaType":"clickWord","token":""}'  \
+   -d '{"captchaType":"clickWord","token":"--"}'  \
    -H "Content-Type: application/json" \
    -H "accept-language: en-US"
 
+echo '\r\n\rtest-check-2,zh_CN'
 curl -X POST http://localhost:8082/captcha/check    \
-   -d '{"captchaType":"clickWord","token":""}'  \
+   -d '{"captchaType":"clickWord","token":"--"}'  \
    -H "Content-Type: application/json" \
    -H "accept-language: zh-CN"
--- service/springboot3/local-docker-run.sh
+++ service/springboot3/local-docker-run.sh
@@ -10,11 +10,18 @@ docker run -d --net host --name captcha-v3 captcha-service:v3
 
 sleep 5
 
-curl -X POST http://localhost:8081/captcha-api/captcha/get \
+curl -X POST http://localhost:8081/captcha/get \
    -d '{"captchaType":"clickWord"}' \
    -H "Content-Type: application/json"
 
+echo '\r\n-test-check-1,en_US'
 curl -X POST http://localhost:8081/captcha/check    \
-   -d '{"captchaType":"clickWord","token":""}'  \
+   -d '{"captchaType":"clickWord","token":"--"}'  \
    -H "Content-Type: application/json" \
    -H "accept-language: en-US"
+
+echo '\r\n-test-check-1,zh-CN'
+curl -X POST http://localhost:8081/captcha/check    \
+   -d '{"captchaType":"clickWord","token":"--"}'  \
+   -H "Content-Type: application/json" \
+   -H "accept-language: zh-CN"
